b3f45ebfad77ee0f9f59c608c860de646702c672
qinghaixiaoge <1308952908@qq.com>
2025-06-04 14:04:18 +0800

    fix:解决切片文件系统占用，删除失败的情况

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const router = express.Router();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./client"));
 app.use("/res", express.static("upload"));
 app.use((req, res, next) => {
   if (req.method === "options") {
@@ -216,5 +217,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`监听${process.env.PORT}端口`);
+  console.log("Server started at http://localhost:3000");
 });
